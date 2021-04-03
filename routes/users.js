fa0bc176518498c24ccb4e8bad9fdd6d60de73a6
Diwakar23 <diwakar232496@gmail.com>
2021-04-03 07:09:49 -0400

    register and login auth

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,2 @@
 let express = require('express');
 let router = express.Router();
-const userController = require("../controllers/user");
-
-router.post("/register", userController.register);
-
-router.post("/login", userController.login);
-
-// router.get('/logout', function (req, res, next) {
-//     req.logout();
-//     return res.status(200).json({ message: 'Logout Success' });
-// })
-
-
-module.exports = router;
